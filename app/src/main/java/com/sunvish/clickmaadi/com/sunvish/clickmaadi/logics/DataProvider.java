b0422eabbc6a8523e653f371e874491a67d8f9bc
Vishwanath K <vishwanathgowdak@gmail.com>
2016-10-02 02:08:33 +0530

    getRandomArrayList completed

--- app/src/main/java/com/sunvish/clickmaadi/com/sunvish/clickmaadi/logics/DataProvider.java
+++ app/src/main/java/com/sunvish/clickmaadi/com/sunvish/clickmaadi/logics/DataProvider.java
@@ -1,8 +1,31 @@
 package com.sunvish.clickmaadi.com.sunvish.clickmaadi.logics;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * Created by kvishwan on 02/10/16.
  */
 public class DataProvider {
 
+
+    public int getScreenHieght(){
+
+        return 0;
+    }
+
+    public int getScreenWidth(){
+
+        return 0;
+    }
+
+    public ArrayList<Integer> getRandomArrayList(int start, int end){
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        for(int i=start;i<end;i++){
+            res.add(i);
+        }
+        Collections.shuffle(res);
+        return res;
+    }
 }
+
---
+++ app/src/test/java/com/sunvish/clickmaadi/DataProviderTest.java
@@ -0,0 +1,27 @@
+package com.sunvish.clickmaadi;
+
+import com.sunvish.clickmaadi.com.sunvish.clickmaadi.logics.DataProvider;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class DataProviderTest {
+
+    DataProvider data = new DataProvider();
+
+    @Before
+    public void setup(){
+
+    }
+
+    @Test
+    public void testgetScreenHeight() throws Exception {
+    }
+
+    public void testgetRandomArrayList() throws Exception{
+
+    }
+}
